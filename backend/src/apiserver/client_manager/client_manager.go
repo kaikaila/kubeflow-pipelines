d491a38c4bcf66997be66270b438148473a6009d
Yunkai Li <54110380+kaikaila@users.noreply.github.com>
2025-06-27 01:04:00 -0700

    Apply suggestions from code review
    Co-authored-by: Humair Khan <HumairAK@users.noreply.github.com>
    Signed-off-by: Yunkai Li <54110380+kaikaila@users.noreply.github.com>

--- backend/src/apiserver/client_manager/client_manager.go
+++ backend/src/apiserver/client_manager/client_manager.go
@@ -339,8 +339,6 @@ func addDisplayNameColumn(db *gorm.DB, quotedTableName string, driverName string
 
 	glog.Info("Adding DisplayName column to " + quotedTableName)
 
-	var err error
-
 	err := db.Transaction(func(tx *gorm.DB) error {
 		var stmts []string
 		switch driverName {
